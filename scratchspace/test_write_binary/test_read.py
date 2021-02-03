a720dba654041fa68dcb3daf0b5350eca7681e5e
Jeremy Magland <magland@users.noreply.github.com>
2021-02-03 15:56:34 -0500

    Update test_read.py

--- scratchspace/test_write_binary/test_read.py
+++ scratchspace/test_write_binary/test_read.py
@@ -1,13 +1,6 @@
 import numpy as np
 
 with open('dd.bin', 'rb') as f:
-    # skip the beginning bytes
-    # discard = np.fromfile(f, count=1, dtype=np.int32)
     x = np.fromfile(f, dtype=np.float64)
 
-# fid = fopen('dd.bin','rb');
-# discard = fread(fid,[1 1],'*int32'); % discard the beginning bytes
-# x = fread(fid,[4],'*double'); % read everything in
-# x
-# fclose(fid);
 print(x)
