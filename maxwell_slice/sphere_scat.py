54c6032c52a43f200a20f14f46e07c43350906c3
Jeremy Magland <jeremy.magland@gmail.com>
2021-02-09 11:25:48 -0500

    misc updates

--- maxwell_slice/sphere_scat.py
+++ maxwell_slice/sphere_scat.py
@@ -2,9 +2,10 @@ import os
 import shutil
 from _temporarydirectory import TemporaryDirectory
 from _shellscript import ShellScript
+from jinja2 import Template
 import numpy as np
 
-def sphere_scat(nnx = 20, nny = 20, nnz = 20):
+def sphere_scat(nnx = 21, nny = 22, nnz = 23):
     # Creates a temporary) working directory (will get cleaned up even if exception is raised)
     # if remove == False, the temporary directory does not get removed
     with TemporaryDirectory(remove=False) as tmpdir:
@@ -14,24 +15,13 @@ def sphere_scat(nnx = 20, nny = 20, nnz = 20):
 
         # generate the input file
         # in future: we'll have a controls.dat.j2 and fill in the args
-        shutil.copyfile(f'{thisdir}/../scratchspace/SphereScat/controls.dat.jinja2', f'{tmpdir}/controls.dat.jinja2')
         shutil.copyfile(f'{thisdir}/../scratchspace/SphereScat/spinsimp.dat', f'{tmpdir}/spinsimp.dat')
 
-        controls = f'{tmpdir}/controls.dat.jinja2'
-        # Read in the file
-        with open(controls, 'r') as file :
-            filedata = file.read()
-
-            # Replace the target string
-        filedata = filedata.replace('xdim', str(nnx))
-        filedata = filedata.replace('ydim', str(nny))
-        filedata = filedata.replace('zdim', str(nnz))
-
-            # Write the file out again
-        with open(controls, 'w') as file:
-            file.write(filedata)
-        print(controls)
-
+        with open(f'{thisdir}/../scratchspace/SphereScat/controls.dat.jinja2', 'r') as f:
+            controls_template = Template(f.read())
+        with open(f'{tmpdir}/controls.dat', 'w') as f:
+            f.write(controls_template.render(nnx=nnx, nny=nny, nnz=nnz))
+        
         # Here is where we run the fortran program using a bash script
         s = ShellScript(f'''
         #!/bin/bash
@@ -48,7 +38,7 @@ def sphere_scat(nnx = 20, nny = 20, nnz = 20):
 
         data = np.fromfile(f'{tmpdir}/data.bin', dtype='>d')
         field = np.fromfile(f'{tmpdir}/field.bin', dtype='>d')
-        data = data.reshape((5, nnx, nny, nnz))
+        data = data.reshape((7, nnx, nny, nnz))
         return data, field
 
 if __name__ == '__main__':
